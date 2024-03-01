561e791fdc6d91e23f12e89f90688950d5857736
maxim.zgardan <maxim.zgardan@accenture.com>
2024-03-01 22:55:47 +0000

    added yaml , upgrade to spring boot 3

--- src/main/java/com/example/kafka/springbootkafkadocker/TestController.java
+++ src/main/java/com/example/kafka/springbootkafkadocker/TestController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private final com.example.kafka.springbootkafka.Producer producer;
+    private final com.example.kafka.springbootkafkadocker.Producer producer;
 
     @Autowired
-    public TestController(com.example.kafka.springbootkafka.Producer producer) {
+    public TestController(com.example.kafka.springbootkafkadocker.Producer producer) {
         this.producer = producer;
     }
     @PostMapping("/publish")
